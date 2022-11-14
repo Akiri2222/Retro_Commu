ffa7e79dbb684bdafacd7173cca22678aa240e44
Ruka2222 <111208324+Akiri2222@users.noreply.github.com>
2022-11-14 13:25:29 +0000

    update

--- app/controllers/user/book_marks_controller.rb
+++ app/controllers/user/book_marks_controller.rb
@@ -1,2 +1,17 @@
 class User::BookMarksController < ApplicationController
+
+  def create
+    post = Post.find(params[:post_id])
+    book_mark = current_user.book_marks.new(post_id: post.id)
+    book_mark.save
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    book_mark = current_user.book_marks.find_by(post_id: post.id)
+    book_mark.destroy
+    redirect_to post_path(post.id)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :genre
+  has_many :comments, dependent: :destroy
+  has_many :book_marks, dependent: :destroy
+
+
+  #ブックマーク済みかどうか判定する
+  def book_marked_by?(user)
+    book_marks.exists?(user_id: user.id)
+  end
+
 
 end
