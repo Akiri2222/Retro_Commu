3e95ecb0b259e0992e075079b8b42daaa6cca109
Ruka2222 <111208324+Akiri2222@users.noreply.github.com>
2022-12-10 13:52:59 +0000

    タグ機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,6 +19,39 @@ class Post < ApplicationRecord
     Post.where('title LIKE ?', '%'+content+'%')
   end
 
+  #タグ付け
+  def save_tags(tags)
+    tags.each do |new_tags|
+      tag = Tag.find_or_create_by(name: new_tags)
+      self.post_tags.find_or_create_by(tag_id: tag.id)
+    end
+  end
+
+  #タグ更新
+  def update_tags(latest_tags)
+    #既存タグの確認
+    tags = Tag.all
+    post_tags = PostTag.all
+    current_tags = self.tags
+    current_tags_name = current_tags.pluck(:name)
+    if self.tags.empty?
+      save_tags(latest_tags)
+    else
+      all_tags = (current_tags_name + latest_tags).uniq.compact
+      delete_tags = all_tags.reject{ |tag| latest_tags.include?(tag) }
+      add_tags = all_tags.reject{ |tag| current_tags_name.include?(tag) }
+      shared_tag_ids = Hash[*current_tags.map{|o| [o.id, o.post_tags.size]}.flatten].select{|k,v|v > 1}.keys
+      delete_tags.each do |tag_name|
+        tag = tags.find_by(name: tag_name)
+        if shared_tag_ids.include?(tag.id)
+          post_tags.find_by(tag_id: tag.id, post_id: self.id)&.destroy
+        else
+          tag.destroy
+        end
+      end
+      save_tags(add_tags)
+    end
+  end
 
 
 end
